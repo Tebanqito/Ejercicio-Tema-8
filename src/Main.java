929464073a700764f657525ae71d6afc5e95dce5
Tebanqito <yemesesteban@gmail.com>
2022-05-30 01:09:37 -0500

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+        persona.setNombre("Esteban");
+        persona.setEdad(21);
+        persona.setTelefono("2376980328");
+
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Telefono: " + persona.getTelefono());
+
+    }
+}
+
+class Persona {
+    private int edad;
+    private String nombre;
+    private String telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
